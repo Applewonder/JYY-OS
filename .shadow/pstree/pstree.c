d869011f11374e31ea9c20897ba038b5ae66c547
tracer-nju <tracer@nju.edu.cn>
2023-03-15 01:01:01 -0700

    Linux ubuntu 5.15.0-67-generic #74~20.04.1-Ubuntu SMP Wed Feb 22 14:52:34 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  01:01:01 up  3:17,  1 user,  load average: 0.00, 0.00, 0.09

--- .shadow/pstree/pstree.c
+++ .shadow/pstree/pstree.c
@@ -145,6 +145,7 @@ int main(int argc, char *argv[]) {
     // }
     if (!strcmp(argv[i], "-V") || !strcmp(argv[i], "--version")) {
       perror("pstree (PSmisc) APPLETREE SPECIAL VERSION\n");
+      fprintf(stderr,"pstree (PSmisc) APPLETREE SPECIAL VERSION\n");
       return 0;
     }
     if (!strcmp(argv[i], "-n") || !strcmp(argv[i], "--numeric-sort")) {
