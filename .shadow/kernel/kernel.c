fcdb3bfb6fcbbb41f426c96505814684ac8bc19f
tracer-nju <tracer@nju.edu.cn>
2023-03-19 07:39:27 -0700

    Linux ubuntu 5.15.0-67-generic #74~20.04.1-Ubuntu SMP Wed Feb 22 14:52:34 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  07:39:27 up 49 min,  0 users,  load average: 0.80, 0.44, 0.19

--- .shadow/kernel/kernel.c
+++ .shadow/kernel/kernel.c
@@ -63,9 +63,9 @@ void splash() {
   
   for (int x = 0; x * SIDE <= w; x ++) {
     for (int y = 0; y * SIDE <= h; y++) {
-      char low = my_array[x*w + y] << 16;
-      char middle = my_array[x*w + y + 1] << 8;
-      char high = my_array[x*w + y + 2] ;
+      char low = my_array[(x*w + y)*3] ;
+      char middle = my_array[(x*w + y)*3 + 1] << 8;
+      char high = my_array[(x*w + y)*3 + 2] << 16 ;
       // if ((x & 1) ^ (y & 1)) {
         draw_tile(x * SIDE, y * SIDE, SIDE, SIDE, 0x000000|high|middle|low); // white
       // }
