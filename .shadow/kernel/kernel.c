6133320133a37f699af09a47f1f8a1923602fa28
tracer-nju <tracer@nju.edu.cn>
2023-03-19 05:33:02 -0700

    Linux ubuntu 5.15.0-67-generic #74~20.04.1-Ubuntu SMP Wed Feb 22 14:52:34 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  05:33:02 up 5 min,  1 user,  load average: 0.27, 0.10, 0.03

--- .shadow/kernel/kernel.c
+++ .shadow/kernel/kernel.c
@@ -62,7 +62,7 @@ void splash() {
   for (int x = 0; x * SIDE <= w; x ++) {
     for (int y = 0; y * SIDE <= h; y++) {
       // if ((x & 1) ^ (y & 1)) {
-        draw_tile(x * SIDE, y * SIDE, SIDE, SIDE, 0xff0000|(x<<8)|y); // white
+        draw_tile(x * SIDE, y * SIDE, SIDE, SIDE, 0x000000|(x<<8)|y); // white
       // }
     }
   }
