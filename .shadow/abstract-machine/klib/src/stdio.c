8a4f1bedf49d8571f2f4d0e9e0c362b34908cb3f
tracer-nju <tracer@nju.edu.cn>
2023-03-22 05:53:54 -0700

    Linux ubuntu 5.15.0-67-generic #74~20.04.1-Ubuntu SMP Wed Feb 22 14:52:34 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  05:53:54 up 1 day, 21:23,  0 users,  load average: 0.16, 0.16, 0.09

--- .shadow/abstract-machine/klib/src/stdio.c
+++ .shadow/abstract-machine/klib/src/stdio.c
@@ -7,7 +7,8 @@
 #if !defined(__ISA_NATIVE__) || defined(__NATIVE_USE_KLIB__)
 
 int printf(const char *fmt, ...) {
-  panic("Not implemented");
+  // panic("Not implemented");
+  return 0;
 }
 
 int vsprintf(char *out, const char *fmt, va_list ap) {
