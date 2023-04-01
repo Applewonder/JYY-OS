268a313d9ab2fc82fbc7e11e63c010ad94fb53fe
tracer-nju <tracer@nju.edu.cn>
2023-04-01 13:33:51 -0700

    Linux ubuntu 5.15.0-69-generic #76~20.04.1-Ubuntu SMP Mon Mar 20 15:54:19 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  13:33:51 up 1 day, 15:52,  0 users,  load average: 0.00, 0.01, 0.00

--- .shadow/plcs/plcs.c
+++ .shadow/plcs/plcs.c
@@ -9,14 +9,16 @@
 #define START_COL 0
 #define START_ROW 1
 #define END_ROW 2
-#define LOCK mutex_lock(&lock)
-#define UNLOCK mutex_unlock(&lock)
+#define LOCK mutex_lock(&lk)
+#define UNLOCK mutex_unlock(&lk)
 int T, N, M;
 char A[MAXN + 1], B[MAXN + 1];
 int dp[MAXN][MAXN];
+int is_dp_filled[MAXN][MAXN];
 int thread_todo_list[MAXN + MAXN][MAX_THREAD][3];
 int result;
-mutex_t lock;
+mutex_t lk = MUTEX_INIT();
+cond_t cv = COND_INIT();
 #define DP(x, y) (((x) >= 0 && (y) >= 0) ? dp[x][y] : 0)
 #define MAX(x, y) (((x) > (y)) ? (x) : (y))
 #define MAX3(x, y, z) MAX(MAX(x, y), z)
@@ -41,6 +43,10 @@ void Tworker(int id) {
   result = dp[N - 1][M - 1];
 }
 
+int is_cond_satisfied(int i, int j) {
+  return (is_dp_filled[i - 1][j] && is_dp_filled[i][j - 1] && is_dp_filled[i - 1][j - 1]);
+}
+
 void Tworker_para(int id) {
   for (int round = 0; round < N + M - 1; round++) {
     int start_col = thread_todo_list[round][id][START_COL];
@@ -52,13 +58,15 @@ void Tworker_para(int id) {
     while (cur_pos + start_row <= end_row) {
       int need_filled_x = start_row + cur_pos;
       int need_filled_y = start_col - cur_pos;
-
+      while (!is_cond_satisfied(need_filled_x, need_filled_y)) {
+        cond_wait(&cv, &lk);
+      }
       LOCK;
       int skip_a = DP(need_filled_x - 1, need_filled_y);
       int skip_b = DP(need_filled_x, need_filled_y - 1);
       int take_both = DP(need_filled_x - 1, need_filled_y - 1) + (A[need_filled_x] == B[need_filled_y]);
       dp[need_filled_x][need_filled_y] = MAX3(skip_a, skip_b, take_both);
-
+      is_dp_filled[need_filled_x][need_filled_y] = 1;
       UNLOCK;
       
       cur_pos ++;
@@ -91,7 +99,6 @@ int main(int argc, char *argv[]) {
     } else if (diagonal_size % T != 0) {
       block_size ++;
     }
-
     for (int i = 1; i <= need_thread_num; i++) {
         int end_row = start_row + block_size - 1;
         if (end_row > diagonal_end_row) {
