e49f3613b9d41e6dc108d4c53b7d70dd37adb04a
tracer-nju <tracer@nju.edu.cn>
2023-04-01 12:51:46 -0700

    Linux ubuntu 5.15.0-69-generic #76~20.04.1-Ubuntu SMP Mon Mar 20 15:54:19 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  12:51:46 up 1 day, 15:10,  0 users,  load average: 0.38, 0.14, 0.04

--- .shadow/plcs/plcs.c
+++ .shadow/plcs/plcs.c
@@ -5,190 +5,107 @@
 #include "thread-sync.h"
 
 #define MAXN 10000
+#define MAX_THREAD 20
+#define START_COL 0
+#define START_ROW 1
+#define END_ROW 2
 #define LOCK mutex_lock(&lock)
 #define UNLOCK mutex_unlock(&lock)
-int T, N, M, L;
-char A[MAXN + 5], B[MAXN + 5];
-int dp[2 * MAXN+5][MAXN+5];
-volatile int val[2 * MAXN+5][MAXN+5];
+int T, N, M;
+char A[MAXN + 1], B[MAXN + 1];
+int dp[MAXN][MAXN];
+int thread_todo_list[MAXN + MAXN][MAX_THREAD][3];
 int result;
-
-#define DP(x, y) (((x) >= 0 && (y) >= 0) ? dp[x+y][y] : 0)
+mutex_t lock;
+#define DP(x, y) (((x) >= 0 && (y) >= 0) ? dp[x][y] : 0)
 #define MAX(x, y) (((x) > (y)) ? (x) : (y))
 #define MAX3(x, y, z) MAX(MAX(x, y), z)
 
-#define MAX_THREAD 20
-int line[20000][MAX_THREAD], left_y[20000][MAX_THREAD], right_y[20000][MAX_THREAD];
-volatile int cango[20000];
-int cntrnd = 0, counter = 0, goon = 1, begin_exit = 0;
-mutex_t lock;
-void FuckYouFuckThisShittyLab(int id) {
-    // printf("P%d enters\n", id);
-    int rnd = 0, dem = 1;
-    while (right_y[rnd][id] != left_y[rnd][id]) {
-        assert(right_y[rnd][id] > left_y[rnd][id]);
-        int l = line[rnd][id], y = left_y[rnd][id], ry = right_y[rnd][id];
-        // printf("P%d %d: %d %d %d\n", id, rnd, l, y, ry);
-        int x = l - y + 1;
-        while (y != ry) {
-            int sa = dp[l][y], sb = (y ? dp[l][y - 1] : 0), sc = ((l && y) ? dp[l - 1][y - 1] : 0) + (A[x] == B[y]);
-            dp[l + 1][y] = MAX3(sa, sb, sc);
-            --x, ++y;
-        }
-        if (id == 1) {
-            LOCK;
-            ++counter;
-            UNLOCK;
+void Tworker(int id) {
+  if (id != 1) {
+    // This is a serial implementation
+    // Only one worker needs to be activated
+    return;
+  }
 
-            /* LOCK; 
-            if (rnd == dem) {
-                printf("P1 entered: counter = %d/%d, rnd = %d\n", counter, T, rnd);
-                dem *= 2;
-            }
-            UNLOCK; */
-            while (1) {
-                LOCK;
-                assert(!begin_exit);
-                if (counter == T) {
-                    --counter;
-                    ++cntrnd;
-                    goon = 0;
-                    UNLOCK;
-                    break;
-                }
-                UNLOCK;
-            }
-            while (1) {
-                LOCK;
-                if (counter == 0) {
-                    goon = 1;
-                    UNLOCK;
-                    break;
-                }
-                UNLOCK;
-            }
-        } else {
-            //LOCK; printf("P2 entered: counter = %d/%d\n", counter, T); UNLOCK;
-            //LOCK; printf("P2 round and goon = %d, %d\n", cntrnd, goon); UNLOCK;
-            while (1) {
-                LOCK;
-                assert(!begin_exit);
-                if (rnd == cntrnd && goon == 1) {
-                    ++counter;
-                    // printf("P2 adds counter = %d/%d\n", counter, T);
-                    UNLOCK;
-                    break;
-                }
-                UNLOCK;
-            }
-            while (1) {
-                LOCK;
-                if (rnd != cntrnd) {
-                    --counter;
-                    UNLOCK;
-                    break;
-                }
-                UNLOCK;
-            }
-        }
-        ++rnd;
-        while (!cango[rnd]);
+  for (int i = 0; i < N; i++) {
+    for (int j = 0; j < M; j++) {
+      // Always try to make DP code more readable
+      int skip_a = DP(i - 1, j);
+      int skip_b = DP(i, j - 1);
+      int take_both = DP(i - 1, j - 1) + (A[i] == B[j]);
+      dp[i][j] = MAX3(skip_a, skip_b, take_both);
     }
-    begin_exit = 1;
-    // printf("P%d exits\n", id);
+  }
+
+  result = dp[N - 1][M - 1];
 }
-int first = 1;
+
+void Tworker_para(int id) {
+  for (int round = 0; round < N + M - 1; round++) {
+    int start_col = thread_todo_list[round][id][START_COL];
+    int start_row = thread_todo_list[round][id][START_ROW];
+    int end_row = thread_todo_list[round][id][END_ROW];
+    assert(start_row <= end_row);
+    int cur_pos = 0;
+    while (cur_pos + start_row <= end_row) {
+      int need_filled_x = start_row + cur_pos;
+      int need_filled_y = start_col - cur_pos;
+
+      LOCK;
+      int skip_a = DP(need_filled_x - 1, need_filled_y);
+      int skip_b = DP(need_filled_x, need_filled_y - 1);
+      int take_both = DP(need_filled_x - 1, need_filled_y - 1) + (A[need_filled_x] == B[need_filled_y]);
+      dp[need_filled_x][need_filled_y] = MAX3(skip_a, skip_b, take_both);
+
+      UNLOCK;
+      
+      cur_pos ++;
+    }
+  }
+}
+
 int main(int argc, char *argv[]) {
-    // No need to change
-    assert(scanf("%s%s", A, B) == 2);
-    N = strlen(A);
-    M = strlen(B);
-    L = MAX(N, M);
-    if (N == 1) {
-        for (int i = 0; i != M; ++i) if (A[0] == B[i]) {
-            printf("1\n");
-            return 0;
-        }
-        printf("0\n");
-        return 0;
-    }
-    //assert(N <= M);
-    T = !argv[1] ? 1 : atoi(argv[1]);
-    // Add preprocessing code here
-    int lx = 1, ly = 0, rx = -1, ry = 2, l = 0;
-    dp[0][0] = (A[0] == B[0]);
-    int count = 0;
-    if (T == 1) {
-        for (int i = 0; i <= 70000000; ++i) {
-            LOCK;
-            UNLOCK;
-            //STUPID LAB, STUPID LOCK
-        }
-    }
-    if (T == 2) {
-        for (int i = 0; i <= 65000000; ++i) {
-            LOCK;
-            UNLOCK;
-            //STUPID LAB, STUPID LOCK
-        }
-    }
-    if (T == 3) {
-        for (int i = 0; i <= 15000000; ++i) {
-            LOCK;
-            UNLOCK;
-            //STUPID LAB, STUPID LOCK
-        }
-    }
-    while (ly != M) {
-        int len = lx - rx, x = lx, y = ly;
-        len /= T;
-        if (len >= 1) {
-            for (int i = 1; i <= T; ++i) {
-                //printf("%d ", x);
+  // No need to change
+  assert(scanf("%s%s", A, B) == 2);
+  N = strlen(A);
+  M = strlen(B);
+  T = !argv[1] ? 1 : atoi(argv[1]);
 
-                line[count][i] = l;
-                left_y[count][i] = y;
-                right_y[count][i] = y + len;
-                if (first) create(FuckYouFuckThisShittyLab);
-                x -= len, y += len;
-            } //printf("%d %d\n", x, rx);
-            right_y[count][T] = ry;
-            cango[count] = 1;
-            ++count;
-            first = 0;
-        } else {
-            if (count) {
-                // printf("process num: %d\n", count);
-                cango[count] = 1;
-                join();
-                count = 0;
-            }
-            while (x != rx) {
-                int sa = dp[l][y], sb = (y ? dp[l][y - 1] : 0), sc = ((l && y) ? dp[l - 1][y - 1] : 0) + (A[x] == B[y]);
-                dp[l + 1][y] = MAX3(sa, sb, sc);
-                --x, ++y;
-            }
-        }
-        if (lx != N - 1) ++lx;
-        else ++ly;
-        if (ry != M) ++ry;
-        else ++rx;
-        ++l;
-    } //printf("%d\n", count);
+  // Add preprocessing code here
+  for (int round = 0; round < N + M - 1; round++) {
+    int diagonal_start_col = (round < M) ? round : M - 1;
+    int diagonal_start_row = (round < M) ? 0 : round - M + 1;
+    int diagonal_end_row = (round < N) ? round : N - 1;
+    int diagonal_size = diagonal_end_row - diagonal_start_row + 1;
 
-    if (count) {
-        // printf("process num: %d\n", count);
-        if (first)
-            for (int i = 1; i <= T; ++i) create(FuckYouFuckThisShittyLab);
-        cango[count] = 1;
-        join();
-        count = 0;
+    int block_size = diagonal_size / T;
+    if (block_size % T != 0) {
+      block_size ++;
     }
-    /* for (int i = 0; i < N; ++i) {
-        for (int j = 0; j < M; ++j) {
-            printf("%d ", dp[i+j][j]);
-        } printf("\n");
-    } */
-    result = dp[N + M - 2][M - 1];
-    printf("%d\n", result);
+
+    int block_size = (diagonal_size + T - 1) / T;
+    int remainder = diagonal_size % T;
+
+    int start_row = diagonal_start_row;
+    int start_col = diagonal_start_col;
+    for (int i = 0; i < T; i++) {
+        int end_row = start_row + block_size - 1;
+        if (end_row > diagonal_end_row) {
+            end_row = diagonal_end_row;
+        }
+        thread_todo_list[round][i][START_COL] = start_col;
+        thread_todo_list[round][i][START_ROW] = start_row;
+        thread_todo_list[round][i][END_ROW] = end_row;
+        start_col = start_col - (end_row - start_row) - 1; 
+        start_row = end_row + 1;
+    }
+  }
+
+  for (int i = 0; i < T; i++) {
+    create(Tworker);
+  }
+  join();  // Wait for all workers
+
+  printf("%d\n", result);
 }
