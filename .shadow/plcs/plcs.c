4d4572c2ac0f88af0ad914d3dac742c0f25449a9
tracer-nju <tracer@nju.edu.cn>
2023-04-03 04:53:01 -0700

    Linux ubuntu 5.15.0-69-generic #76~20.04.1-Ubuntu SMP Mon Mar 20 15:54:19 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  04:53:01 up 3 days,  7:11,  0 users,  load average: 0.12, 0.04, 0.01

--- .shadow/plcs/plcs.c
+++ .shadow/plcs/plcs.c
@@ -100,7 +100,7 @@ void Tworker_cache_para(int id) {
       // LOCK;
       dp_cache[need_filled_x][need_filled_y] = MAX3(skip_a, skip_b, take_both); BARRIER;
       is_dp_cache_filled[need_filled_x][need_filled_y] = 1; BARRIER;
-      printf("The dp[%d][%d]'s value is %d", start_row + cur_pos, start_col - cur_pos, dp_cache[need_filled_x][need_filled_y]);
+      printf("The dp[%d][%d]'s value is %d\n", start_row + cur_pos, start_col - cur_pos, dp_cache[need_filled_x][need_filled_y]);
       // UNLOCK;
       //printf("I'm in thread %d, round %d, fill the diaganol %d succesfully\n", id, round, cur_pos);
       cond_broadcast(&cv);
