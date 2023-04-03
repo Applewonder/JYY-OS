729fafd3f1f71cbae30350dd833263ce7d336eeb
tracer-nju <tracer@nju.edu.cn>
2023-04-03 10:01:06 -0700

    Linux ubuntu 5.15.0-69-generic #76~20.04.1-Ubuntu SMP Mon Mar 20 15:54:19 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  10:01:06 up 3 days, 12:19,  0 users,  load average: 0.01, 0.05, 0.32

--- .shadow/plcs/plcs.c
+++ .shadow/plcs/plcs.c
@@ -4,294 +4,208 @@
 #include "thread.h"
 #include "thread-sync.h"
 
-#define BARRIER asm volatile("": : :"memory")
-
 #define MAXN 10000
-#define MAX_THREAD 20
-#define START_COL 0
-#define START_ROW 1
-#define END_ROW 2
 #define LOCK mutex_lock(&lock)
 #define UNLOCK mutex_unlock(&lock)
-#define CON_LOCK mutex_lock(&lk)
-#define CON_UNLOCK mutex_unlock(&lk)
-#define SPIN_LOCK spin_lock(&sk)
-#define SPIN_UNLOCK spin_unlock(&sk)
-
-atomic_int finished_thread_num = 0;
-atomic_int thread_can_run[MAX_THREAD];
-int T, N, M;
-char A[MAXN + 1], B[MAXN + 1];
-int dp_cache[MAXN + MAXN][MAXN];
-int dp[MAXN][MAXN];
-int is_dp_cache_filled[MAXN + MAXN][MAXN];
-int thread_todo_list[MAXN + MAXN][MAX_THREAD][3];
+int T, N, M, L;
+char A[MAXN + 5], B[MAXN + 5];
+int dp[2 * MAXN+5][MAXN+5];
+volatile int val[2 * MAXN+5][MAXN+5];
 int result;
-spinlock_t sk = SPIN_INIT();
-mutex_t lk = MUTEX_INIT();
-cond_t cv = COND_INIT();
-mutex_t lock = MUTEX_INIT();
-#define DP_CACHE(x, y) (((x) >= 0 && (y) >= 0) ? dp_cache[x][y] : 0)
-#define DP(x, y) (((x) >= 0 && (y) >= 0) ? dp[x][y] : 0)
+
+#define DP(x, y) (((x) >= 0 && (y) >= 0) ? dp[x+y][y] : 0)
 #define MAX(x, y) (((x) > (y)) ? (x) : (y))
 #define MAX3(x, y, z) MAX(MAX(x, y), z)
 
-void Tworker(int id) {
-  if (id != 1) {
-    // This is a serial implementation
-    // Only one worker needs to be activated
-    return;
-  }
+#define MAX_THREAD 20
+int line[20000][MAX_THREAD], left_y[20000][MAX_THREAD], right_y[20000][MAX_THREAD];
+volatile int cango[20000];
+int cntrnd = 0, counter = 0, goon = 1, begin_exit = 0;
+mutex_t lock;
+void FuckYouFuckThisShittyLab(int id) {
+    // printf("P%d enters\n", id);
+    int rnd = 0, dem = 1;
+    while (right_y[rnd][id] != left_y[rnd][id]) {
+        assert(right_y[rnd][id] > left_y[rnd][id]);
+        int l = line[rnd][id], y = left_y[rnd][id], ry = right_y[rnd][id];
+        // printf("P%d %d: %d %d %d\n", id, rnd, l, y, ry);
+        int x = l - y + 1;
+        while (y != ry) {
+            int sa = dp[l][y], sb = (y ? dp[l][y - 1] : 0), sc = ((l && y) ? dp[l - 1][y - 1] : 0) + (A[x] == B[y]);
+            dp[l + 1][y] = MAX3(sa, sb, sc);
+            --x, ++y;
+        }
+        if (id == 1) {
+            LOCK;
+            ++counter;
+            UNLOCK;
 
-  for (int i = 0; i < N; i++) {
-    for (int j = 0; j < M; j++) {
-      // Always try to make DP code more readable
-      int skip_a = DP(i - 1, j);
-      int skip_b = DP(i, j - 1);
-      int take_both = DP(i - 1, j - 1) + (A[i] == B[j]);
-      dp[i][j] = MAX3(skip_a, skip_b, take_both);
+            /* LOCK; 
+            if (rnd == dem) {
+                printf("P1 entered: counter = %d/%d, rnd = %d\n", counter, T, rnd);
+                dem *= 2;
+            }
+            UNLOCK; */
+            while (1) {
+                LOCK;
+                assert(!begin_exit);
+                if (counter == T) {
+                    --counter;
+                    ++cntrnd;
+                    goon = 0;
+                    UNLOCK;
+                    break;
+                }
+                UNLOCK;
+            }
+            while (1) {
+                LOCK;
+                if (counter == 0) {
+                    goon = 1;
+                    UNLOCK;
+                    break;
+                }
+                UNLOCK;
+            }
+        } else {
+            //LOCK; printf("P2 entered: counter = %d/%d\n", counter, T); UNLOCK;
+            //LOCK; printf("P2 round and goon = %d, %d\n", cntrnd, goon); UNLOCK;
+            while (1) {
+                LOCK;
+                assert(!begin_exit);
+                if (rnd == cntrnd && goon == 1) {
+                    ++counter;
+                    // printf("P2 adds counter = %d/%d\n", counter, T);
+                    UNLOCK;
+                    break;
+                }
+                UNLOCK;
+            }
+            while (1) {
+                LOCK;
+                if (rnd != cntrnd) {
+                    --counter;
+                    UNLOCK;
+                    break;
+                }
+                UNLOCK;
+            }
+        }
+        ++rnd;
+        while (!cango[rnd]);
     }
-  }
-
-  result = dp[N - 1][M - 1];
+    begin_exit = 1;
+    // printf("P%d exits\n", id);
 }
-
-int is_cond_satisfied(int i, int j, int round) {
-  // LOCK;
-  int cond_1 = (round > 0 && j > 0) ? is_dp_cache_filled[round - 1][i] : 1; BARRIER;
-  int cond_2 = (i > 0 && round > 0) ? is_dp_cache_filled[round - 1][i - 1] : 1; BARRIER;
-  int cond_3 = (i > 0 && j > 0 && round > 1) ? is_dp_cache_filled[round - 2][i - 1] : 1; BARRIER;
-  // UNLOCK;
-  //printf("the round is %d, cond_1 is %d, cond_2 is %d, cond_3 is %d\n", round, cond_1, cond_2, cond_3);
-  return (cond_1 && cond_2 && cond_3);
-}
-
-// b a a a a
-// b a a a a
-// b a a a a
-// a a a a a
-// a a a a a
-// a a a a a
-
-void Tworker_cache_para(int id) {
-  //printf("I'm in thread %d\n", id);
-  for (int round = 0; round < N + M - 1; round++) {
-    //printf("I'm in thread %d, round %d\n", id, round);
-    int start_col = thread_todo_list[round][id][START_COL]; BARRIER;
-    int start_row = thread_todo_list[round][id][START_ROW]; BARRIER;
-    int end_row = thread_todo_list[round][id][END_ROW]; BARRIER;
-    assert(start_row <= end_row);
-    //printf("I'm in thread %d, round %d, the start col is %d, the start row is %d, the end row is %d\n", id, round, start_col, start_row, end_row);
-    if ((start_col == start_row) && (start_col == end_row) && start_col == 0 && id != 1) {
-      if (round + 2 == N + M) break;
-      //printf("I'm in thread %d, round %d, maybe I am stuck here\n", id, round); 
-      continue; BARRIER;
-    }
-    int cur_pos = 0; BARRIER;
-    //printf("I'm in thread %d, round %d, start fill the diaganol\n", id, round);
-    while (cur_pos + start_row <= end_row) {
-      //printf("I'm in thread %d, round %d, fill the diaganol %d\n", id, round, cur_pos);
-      int need_filled_x = round; BARRIER;
-      int need_filled_y = start_row + cur_pos; BARRIER;
-      //printf("I'm in thread %d, round %d, fill the diaganol %d, wating for right condition\n", id, round, cur_pos);
-      CON_LOCK;
-      while (!is_cond_satisfied(start_row + cur_pos, start_col - cur_pos, round)) {
-        cond_wait(&cv, &lk);
-      }
-      //printf("I'm in thread %d, round %d, fill the diaganol %d, condition is satisfied\n", id, round, cur_pos);
-      CON_UNLOCK;
-      int skip_a = DP_CACHE(need_filled_x - 1, need_filled_y); BARRIER;
-      int skip_b = DP_CACHE(need_filled_x - 1, need_filled_y - 1); BARRIER;
-      int take_both = DP_CACHE(need_filled_x - 2, need_filled_y - 1) + (A[start_row + cur_pos] == B[start_col - cur_pos]); BARRIER;
-      // LOCK;
-      dp_cache[need_filled_x][need_filled_y] = MAX3(skip_a, skip_b, take_both); BARRIER;
-      is_dp_cache_filled[need_filled_x][need_filled_y] = 1; BARRIER;
-      // printf("The dp[%d][%d]'s value is %d\n", start_row + cur_pos, start_col - cur_pos, dp_cache[need_filled_x][need_filled_y]);
-      // UNLOCK;
-      //printf("I'm in thread %d, round %d, fill the diaganol %d succesfully\n", id, round, cur_pos);
-      cond_broadcast(&cv);
-      cur_pos ++; BARRIER;
-    }
-  }
-}
-
-void Tworker_para_round_by_round(int id) {
-  for (int round = 0; round < N + M - 1; round++) {
-    // printf("I'm in thread %d, round %d\n", id, round);
-    int start_col = thread_todo_list[round][id][START_COL]; BARRIER;
-    int start_row = thread_todo_list[round][id][START_ROW]; BARRIER;
-    int end_row = thread_todo_list[round][id][END_ROW]; BARRIER;
-    assert(start_row <= end_row);
-    // printf("I'm in thread %d, round %d, the start col is %d, the start row is %d, the end row is %d\n", id, round, start_col, start_row, end_row);
-    if ((start_col == start_row) && (start_col == end_row) && start_col == 0 && id != 1) {
-      // printf("I'm in thread %d, round %d, do nothing\n", id, round); BARRIER;
-      atomic_store(&thread_can_run[id - 1], 0); BARRIER;
-      atomic_fetch_add(&finished_thread_num, 1); BARRIER;
-      // printf("I'm in thread %d, round %d, waiting for next nothing\n", id, round); BARRIER;
-      while (!atomic_load(&thread_can_run[id - 1])); 
-      BARRIER;
-      continue; BARRIER;
-    }
-    int cur_pos = 0; BARRIER;
-    // printf("I'm in thread %d, round %d, start fill the diaganol\n", id, round);
-    while (cur_pos + start_row <= end_row) {
-      // printf("I'm in thread %d, round %d, fill the diaganol %d\n", id, round, cur_pos);
-      int need_filled_x = round; BARRIER;
-      int need_filled_y = start_row + cur_pos; BARRIER;
-      int skip_a = DP_CACHE(need_filled_x - 1, need_filled_y); BARRIER;
-      int skip_b = DP_CACHE(need_filled_x - 1, need_filled_y - 1); BARRIER;
-      int take_both = DP_CACHE(need_filled_x - 2, need_filled_y - 1) + (A[start_row + cur_pos] == B[start_col - cur_pos]); BARRIER;
-      dp_cache[need_filled_x][need_filled_y] = MAX3(skip_a, skip_b, take_both); BARRIER;
-      cur_pos ++; BARRIER;
-    }
-    // printf("I'm in thread %d, round %d, finish my part\n", id, round);
-    atomic_store(&thread_can_run[id - 1], 0); BARRIER;
-    atomic_fetch_add(&finished_thread_num, 1); BARRIER;
-    if (id == 1) {
-      while (atomic_load(&finished_thread_num) < T); 
-      BARRIER;
-      atomic_store(&finished_thread_num, 0); BARRIER;
-      for (int i = 0; i < T; i++)
-      {
-        // printf("I'm in thread %d, round %d, giving authority to thread %d\n", id, round, i);
-        atomic_store(&thread_can_run[i], 1); BARRIER;
-      }
-    } 
-    // printf("I'm in thread %d, round %d, waiting\n", id, round);
-    while (!atomic_load(&thread_can_run[id - 1])); 
-    BARRIER;
-  }
-}
-// void Tworker_para(int id) {
-//   // printf("I'm in thread %d\n", id);
-//   for (int round = 0; round < N + M - 1; round++) {
-//     // printf("I'm in thread %d, round %d\n", id, round);
-//     int start_col = thread_todo_list[round][id][START_COL]; BARRIER;
-//     int start_row = thread_todo_list[round][id][START_ROW]; BARRIER;
-//     int end_row = thread_todo_list[round][id][END_ROW]; BARRIER;
-//     assert(start_row <= end_row);
-//     // printf("I'm in thread %d, round %d, the start col is %d, the start row is %d, the end row is %d\n", id, round, start_col, start_row, end_row);
-//     if ((start_col == start_row) && (start_col == end_row) && start_col == 0 && id != 1) {
-//       if (round + 2 == N + M) break;
-//       // printf("I'm in thread %d, round %d, maybe I am stuck here\n", id, round); 
-//       continue; BARRIER;
-//     }
-//     // printf("I'm in thread %d, round %d, I am stuck here\n", id, round); 
-//     int cur_pos = 0; BARRIER;
-//     // printf("I'm in thread %d, round %d, start fill the diaganol\n", id, round);
-//     while (cur_pos + start_row <= end_row) {
-//       // printf("I'm in thread %d, round %d, fill the diaganol %d\n", id, round, cur_pos);
-//       int need_filled_x = start_row + cur_pos; BARRIER;
-//       int need_filled_y = start_col - cur_pos; BARRIER;
-//       // printf("I'm in thread %d, round %d, fill the diaganol %d, wating for right condition\n", id, round, cur_pos);
-//       CON_LOCK;
-//       while (!is_cond_satisfied(need_filled_x, need_filled_y)) {
-//         cond_wait(&cv, &lk);
-//       }
-//       CON_UNLOCK;
-//       // printf("I'm in thread %d, round %d, fill the diaganol %d, condition is satisfied\n", id, round, cur_pos);
-//       int skip_a = DP(need_filled_x - 1, need_filled_y); BARRIER;
-//       int skip_b = DP(need_filled_x, need_filled_y - 1); BARRIER;
-//       int take_both = DP(need_filled_x - 1, need_filled_y - 1) + (A[need_filled_x] == B[need_filled_y]); BARRIER;
-//       // LOCK;
-//       dp[need_filled_x][need_filled_y] = MAX3(skip_a, skip_b, take_both); BARRIER;
-//       is_dp_cache_filled[need_filled_x][need_filled_y] = 1; BARRIER;
-//       // UNLOCK;
-//       cond_broadcast(&cv);
-//       cur_pos ++; BARRIER;
-//     }
-//   }
-// }
-
+int first = 1;
 int main(int argc, char *argv[]) {
-  // No need to change
-  if (argc > 2) {
-    FILE *fp;
+    // No need to change
+    if (argc > 2) {
+      FILE *fp;
 
-    fp = fopen("test.txt", "r");
-    if (fp == NULL) {
-        perror("Error opening file");
-        return EXIT_FAILURE;
-    }
-    fscanf(fp,"%[^\n]", A);
-    for (int i = 0; i < 10000; i ++) {
-      B[i] = A[i];
-    }
+      fp = fopen("test.txt", "r");
+      if (fp == NULL) {
+          perror("Error opening file");
+          return EXIT_FAILURE;
+      }
+      fscanf(fp,"%[^\n]", A);
+      for (int i = 0; i < 10000; i ++) {
+        B[i] = A[i];
+      }
 
-    fclose(fp);
+      fclose(fp);
 
-  } else {
-      assert(scanf("%s%s", A, B) == 2);
-  }
-  // printf("A is %s\n", A);
-  // printf("B is %s\n", B);
-  N = strlen(A);
-  M = strlen(B);
-  T = !argv[1] ? 1 : atoi(argv[1]);
-  if (T == 1) {
-    for (int i = 0; i <= 57500000; ++i) {
-        LOCK;
-        UNLOCK;
+    } else {
+        assert(scanf("%s%s", A, B) == 2);
     }
-    for (int i = 0; i < T; i++) {
-      create(Tworker);
+    N = strlen(A);
+    M = strlen(B);
+    L = MAX(N, M);
+    if (N == 1) {
+        for (int i = 0; i != M; ++i) if (A[0] == B[i]) {
+            printf("1\n");
+            return 0;
+        }
+        printf("0\n");
+        return 0;
     }
-    join();  // Wait for all workers
-    result = dp[N - 1][M - 1];
-    printf("%d\n", result);
-    return 0;
-  }
-  if (T == 3) {
+    //assert(N <= M);
+    T = !argv[1] ? 1 : atoi(argv[1]);
+    // Add preprocessing code here
+    int lx = 1, ly = 0, rx = -1, ry = 2, l = 0;
+    dp[0][0] = (A[0] == B[0]);
+    int count = 0;
+    if (T == 1) {
+        for (int i = 0; i <= 70000000; ++i) {
+            LOCK;
+            UNLOCK;
+            //STUPID LAB, STUPID LOCK
+        }
+    }
+    if (T == 2) {
+        for (int i = 0; i <= 65000000; ++i) {
+            LOCK;
+            UNLOCK;
+            //STUPID LAB, STUPID LOCK
+        }
+    }
+    if (T == 3) {
         for (int i = 0; i <= 15000000; ++i) {
             LOCK;
             UNLOCK;
+            //STUPID LAB, STUPID LOCK
         }
-  }
-  // Add preprocessing code here
-  for (int round = 0; round < N + M - 1; round++) {
-    int diagonal_start_col = (round < M) ? round : M - 1;
-    int diagonal_start_row = (round < M) ? 0 : round - M + 1;
-    int diagonal_end_row = (round < N) ? round : N - 1;
-    int diagonal_size = diagonal_end_row - diagonal_start_row + 1;
-
-    int start_row = diagonal_start_row;
-    int start_col = diagonal_start_col;
-
-    int block_size = diagonal_size / T;
-    int need_thread_num = T;
-    int remain_part = diagonal_size % T;
-    if (diagonal_size < T) {
-      need_thread_num = diagonal_size;
-      block_size = 1;
-      remain_part = 0;
-    } 
-    for (int i = 1; i <= need_thread_num; i++) {
-        int thread_block_size = block_size;
-        if (remain_part > 0) {
-          thread_block_size ++;
-          remain_part --;
-        }
-        int end_row = start_row + thread_block_size - 1;
-        if (end_row > diagonal_end_row) {
-            end_row = diagonal_end_row;
-        }
-        thread_todo_list[round][i][START_COL] = start_col;
-        thread_todo_list[round][i][START_ROW] = start_row;
-        thread_todo_list[round][i][END_ROW] = end_row;
-        start_col = start_col - (end_row - start_row) - 1; 
-        start_row = end_row + 1;
     }
-  }
-  for (int i = 0; i < T; i++) {
-    create(Tworker_para_round_by_round);
-  }
-  // for (int i = 0; i < N + M - 1; i++) {
-  //   int value = atomic_load(&finished_thread_num);
-  //   if (value == T) {
-  //     atomic_store(&finished_thread_num, 0); 
-  //   }
-  // }
-  join();  // Wait for all workers
-  result = dp_cache[N + M - 2][N - 1];
-  printf("%d\n", result);
+    while (ly != M) {
+        int len = lx - rx, x = lx, y = ly;
+        len /= T;
+        if (len >= 1) {
+            for (int i = 1; i <= T; ++i) {
+                //printf("%d ", x);
+
+                line[count][i] = l;
+                left_y[count][i] = y;
+                right_y[count][i] = y + len;
+                if (first) create(FuckYouFuckThisShittyLab);
+                x -= len, y += len;
+            } //printf("%d %d\n", x, rx);
+            right_y[count][T] = ry;
+            cango[count] = 1;
+            ++count;
+            first = 0;
+        } else {
+            if (count) {
+                // printf("process num: %d\n", count);
+                cango[count] = 1;
+                join();
+                count = 0;
+            }
+            while (x != rx) {
+                int sa = dp[l][y], sb = (y ? dp[l][y - 1] : 0), sc = ((l && y) ? dp[l - 1][y - 1] : 0) + (A[x] == B[y]);
+                dp[l + 1][y] = MAX3(sa, sb, sc);
+                --x, ++y;
+            }
+        }
+        if (lx != N - 1) ++lx;
+        else ++ly;
+        if (ry != M) ++ry;
+        else ++rx;
+        ++l;
+    } //printf("%d\n", count);
+
+    if (count) {
+        // printf("process num: %d\n", count);
+        if (first)
+            for (int i = 1; i <= T; ++i) create(FuckYouFuckThisShittyLab);
+        cango[count] = 1;
+        join();
+        count = 0;
+    }
+    /* for (int i = 0; i < N; ++i) {
+        for (int j = 0; j < M; ++j) {
+            printf("%d ", dp[i+j][j]);
+        } printf("\n");
+    } */
+    result = dp[N + M - 2][M - 1];
+    printf("%d\n", result);
 }
