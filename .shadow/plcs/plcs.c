d0c3f2964afac5302382f27be3ea3155f3e37187
tracer-nju <tracer@nju.edu.cn>
2023-04-03 04:59:51 -0700

    Linux ubuntu 5.15.0-69-generic #76~20.04.1-Ubuntu SMP Mon Mar 20 15:54:19 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  04:59:51 up 3 days,  7:18,  0 users,  load average: 0.07, 0.03, 0.00

--- .shadow/plcs/plcs.c
+++ .shadow/plcs/plcs.c
@@ -161,7 +161,9 @@ int main(int argc, char *argv[]) {
         return EXIT_FAILURE;
     }
     fscanf(fp,"%[^\n]", A);
-    fscanf(fp,"%[^\n]", B);
+    for (int i = 0; i < 10000; i ++) {
+      B[i] = A[i];
+    }
 
     fclose(fp);
 
