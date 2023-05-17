08d4d4370596e197a7ef14f8c656557a9318ba7f
tracer-nju <tracer@nju.edu.cn>
2023-05-17 20:53:06 +0800

    Linux appletree-virtual-machine 5.19.0-41-generic #42~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Apr 18 17:40:00 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  20:53:06 up 10 days, 19:45,  1 user,  load average: 0.20, 0.22, 0.25

--- .shadow/kernel/framework/main.c
+++ .shadow/kernel/framework/main.c
@@ -4,11 +4,22 @@
 int main() {
   os->init();
   mpe_init(os->run);
-#ifdef TEST
-``for (size_t i = 0; i < 10000; i++)
-{
-  kmalloc(i);
-}
+#ifdef NTEST
+  for (size_t i = 0; i < 10000; i++)
+  {
+    void* int32_ptr = kmalloc(32);
+    void* int64_ptr = kmalloc(64); 
+    void* int128_ptr = kmalloc(128);
+    void* int256_ptr = kmalloc(256);
+    void* int512_ptr = kmalloc(512);
+    void* int1024_ptr = kmalloc(1024);
+    kfree(int32_ptr);
+    kfree(int64_ptr);
+    kfree(int128_ptr);
+    kfree(int256_ptr);
+    kfree(int512_ptr);
+    kfree(int1024_ptr);
+  }
 
 
 #endif
