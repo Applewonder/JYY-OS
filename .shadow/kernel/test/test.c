a871227fbc2dc27966a0f5aca37dde4832c0c34b
tracer-nju <tracer@nju.edu.cn>
2023-05-18 14:44:12 +0800

    Linux appletree-virtual-machine 5.19.0-41-generic #42~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Apr 18 17:40:00 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  14:44:12 up 11 days, 13:36,  1 user,  load average: 0.34, 0.63, 0.62

--- .shadow/kernel/test/test.c
+++ .shadow/kernel/test/test.c
@@ -9,26 +9,46 @@ static void entry(int tid) {
   printf("thread_id[%d]: %ld\n", cur_cpu, thread_id[cur_cpu]);
   for (int i = 0; i < 10000; i++)
   {
-    void* int32_ptr = pmm->alloc(32);
-    printf("int32_ptr: %p\n", int32_ptr);
-    void* int64_ptr = pmm->alloc(64);
-    printf("int64_ptr: %p\n", int64_ptr);
-    void* int128_ptr = pmm->alloc(128);
-    printf("int128_ptr: %p\n", int128_ptr);
-    void* int256_ptr = pmm->alloc(256);
-    printf("int256_ptr: %p\n", int256_ptr);
-    void* int512_ptr = pmm->alloc(512);
-    printf("int512_ptr: %p\n", int512_ptr);
-    void* int1024_ptr = pmm->alloc(1024);
-    printf("int1024_ptr: %p\n", int1024_ptr);
-    pmm->free(int32_ptr);
-    pmm->free(int64_ptr);
-    pmm->free(int128_ptr);
-    pmm->free(int256_ptr);
-    pmm->free(int512_ptr);
-    pmm->free(int1024_ptr);
+    int choose_type = i % 6;
+    switch(choose_type){
+      case 0: {
+        void* int32_ptr = pmm->alloc(32);
+        printf("int32_ptr: %p\n", int32_ptr);
+              pmm->free(int32_ptr);
+        break;
+      }
+      case 1: {
+        void* int64_ptr = pmm->alloc(64);
+        printf("int64_ptr: %p\n", int64_ptr);
+        pmm->free(int64_ptr);
+        break;
+      }
+      case 2: {
+        void* int128_ptr = pmm->alloc(128);
+        printf("int128_ptr: %p\n", int128_ptr);
+        pmm->free(int128_ptr);
+        break;
+      }
+      case 3: {
+        void* int256_ptr = pmm->alloc(256);
+        printf("int256_ptr: %p\n", int256_ptr);
+        pmm->free(int256_ptr);
+        break;
+      }
+      case 4: {
+        void* int512_ptr = pmm->alloc(512);
+        printf("int512_ptr: %p\n", int512_ptr);
+        pmm->free(int512_ptr);
+        break;
+      }
+      case 5: {
+        void* int1024_ptr = pmm->alloc(1024);
+        printf("int1024_ptr: %p\n", int1024_ptr);
+        pmm->free(int1024_ptr);
+        break;
+      }
+    }
   }
-  
 }
 static void goodbye()      { printf("End.\n"); }
 int main() {
