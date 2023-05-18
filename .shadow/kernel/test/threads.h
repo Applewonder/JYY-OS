d30ecd58d8d1282eda1a688fb320e2eca0ca076f
tracer-nju <tracer@nju.edu.cn>
2023-05-18 18:05:14 +0800

    Linux appletree-virtual-machine 5.19.0-41-generic #42~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Apr 18 17:40:00 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  18:05:14 up 11 days, 16:57,  1 user,  load average: 1.20, 0.65, 0.44

--- .shadow/kernel/test/threads.h
+++ .shadow/kernel/test/threads.h
@@ -17,42 +17,19 @@ struct thread {
   void (*entry)(int);
 };
 
-struct thread tpool[NTHREAD], *tptr = tpool;
 
-void *wrapper(void *arg) {
-  struct thread *thread = (struct thread *)arg;
-  thread->entry(thread->id);
-  return NULL;
-}
 
-void create(void *fn) {
-  assert(tptr - tpool < NTHREAD);
-  *tptr = (struct thread) {
-    .id = tptr - tpool + 1,
-    .status = T_LIVE,
-    .entry = fn,
-  };
-  pthread_create(&(tptr->thread), NULL, wrapper, tptr);
-  ++tptr;
-}
+void *wrapper(void *arg);
 
-void join() {
-  for (int i = 0; i < NTHREAD; i++) {
-    struct thread *t = &tpool[i];
-    if (t->status == T_LIVE) {
-      pthread_join(t->thread, NULL);
-      t->status = T_DEAD;
-    }
-  }
-}
+void create(void *fn);
 
-__attribute__((destructor)) void cleanup() {
-  join();
-}
+void join();
+
+void cleanup();
 
 typedef pthread_mutex_t mutex_t;
 #define MUTEX_INIT() PTHREAD_MUTEX_INITIALIZER
-void mutex_lock(mutex_t *lk)   { pthread_mutex_lock(lk); }
-void mutex_unlock(mutex_t *lk) { pthread_mutex_unlock(lk); }
+void mutex_lock(mutex_t *lk);
+void mutex_unlock(mutex_t *lk);
 
 #endif
