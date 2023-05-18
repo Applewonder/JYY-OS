dc02bc4f2d232cfc8cf23a488de0d9fc13ddb661
tracer-nju <tracer@nju.edu.cn>
2023-05-18 14:31:09 +0800

    Linux appletree-virtual-machine 5.19.0-41-generic #42~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Apr 18 17:40:00 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  14:31:09 up 11 days, 13:23,  1 user,  load average: 0.39, 0.37, 0.31

--- .shadow/kernel/test/test.c
+++ .shadow/kernel/test/test.c
@@ -7,7 +7,22 @@ static void entry(int tid) {
   int cur_cpu = tid - 1;
   thread_id[cur_cpu] = pthread_self();
   printf("thread_id[%d]: %ld\n", cur_cpu, thread_id[cur_cpu]);
-  pmm->alloc(128);
+  for (int i = 0; i < 10000; i++)
+  {
+    void* int32_ptr = pmm->alloc(32);
+    void* int64_ptr = pmm->alloc(64);
+    void* int128_ptr = pmm->alloc(128);
+    void* int256_ptr = pmm->alloc(256);
+    void* int512_ptr = pmm->alloc(512);
+    void* int1024_ptr = pmm->alloc(1024);
+    pmm->free(int32_ptr);
+    pmm->free(int64_ptr);
+    pmm->free(int128_ptr);
+    pmm->free(int256_ptr);
+    pmm->free(int512_ptr);
+    pmm->free(int1024_ptr);
+  }
+  
 }
 static void goodbye()      { printf("End.\n"); }
 int main() {
