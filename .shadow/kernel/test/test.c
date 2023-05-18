eb497339e27041d933f6fb583aa0651989da4136
tracer-nju <tracer@nju.edu.cn>
2023-05-18 19:54:31 +0800

    Linux appletree-virtual-machine 5.19.0-41-generic #42~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Apr 18 17:40:00 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  19:54:31 up 11 days, 18:46,  1 user,  load average: 0.45, 0.42, 0.38

--- .shadow/kernel/test/test.c
+++ .shadow/kernel/test/test.c
@@ -91,9 +91,7 @@ void judger_for_test_0() {
     int line_num = 0;
     printf("Judger for test_0\n");
     file = fopen("/home/appletree/JYY-OS/kernel/test/testlog.txt", "r");
-    size_t bufferSize = MAX_LINE_LENGTH;
-    int bytesRead = 0;
-    while ((bytesRead = getline(&line, &bufferSize, file)) != -1) {
+    while (fgets(line, sizeof(line), file) != NULL) {
         if (line[0] == 'A') {
             int start_index = 6;
             int end_index = find_the_target_index(line);
