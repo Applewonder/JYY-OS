d71699f0b227e2dc506d37ff1f2d9d31eb6a1983
tracer-nju <tracer@nju.edu.cn>
2023-05-22 20:50:43 +0800

    Linux appletree-virtual-machine 5.19.0-41-generic #42~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Apr 18 17:40:00 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  20:50:43 up 15 days, 19:42,  1 user,  load average: 0.67, 0.65, 0.85

--- .shadow/kernel/test/testcase.c
+++ .shadow/kernel/test/testcase.c
@@ -168,7 +168,7 @@ void do_test_1() {
     file = fopen("/home/appletree/JYY-OS/kernel/test/testlog1.txt", "w");
     fclose(file);
     pmm->init();
-    for (int i = 0; i < 3; i++){
+    for (int i = 0; i < 6; i++){
         create(entry_1);
     }
     join();
