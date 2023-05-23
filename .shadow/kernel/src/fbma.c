a7fd1a5791a5ec4f71572e260c1ecf74d4008c6e
tracer-nju <tracer@nju.edu.cn>
2023-05-23 23:58:22 +0800

    Linux appletree-virtual-machine 5.19.0-41-generic #42~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Apr 18 17:40:00 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  23:58:22 up 16 days, 22:50,  0 users,  load average: 0.44, 0.51, 0.68

--- .shadow/kernel/src/fbma.c
+++ .shadow/kernel/src/fbma.c
@@ -17,7 +17,7 @@ typedef pthread_mutex_t mutex_t;
 BUDDY_BLOCK_STICK* buddy_blocks[BBMA_NUM];
 // spinlock_t bbma_lock[BBMA_NUM];
 spinlock_t bbma_lock;
-mutex_t bbma_mutex = MUTEX_INIT();
+extern mutex_t mutex;
 
 BUDDY_BLOCK_SIZE determine_bbma_size(size_t size) {
     size_t real_size = size;
@@ -64,7 +64,7 @@ void* get_the_free_space_by_dividing(BUDDY_BLOCK_SIZE bbma_size) {
 }
 
 void* bbma_alloc(size_t size, bool is_from_slab) {
-    mutex_lock(&bbma_mutex);
+    mutex_lock(&mutex);
     BUDDY_BLOCK_SIZE bbma_size = BBMA_REFUSE;
     if (is_from_slab) {
         if (size != SLAB_REQUEST_SPACE) {
@@ -95,7 +95,7 @@ void* bbma_alloc(size_t size, bool is_from_slab) {
 #ifdef TEST
     // assert()
 #endif
-    mutex_unlock(&bbma_mutex);
+    mutex_unlock(&mutex);
     return possible_bbma_addr;
 }
 
@@ -325,7 +325,7 @@ void spy_insert_chain_block(BUDDY_BLOCK_STICK* item) {
 }
 
 void bbma_free(void* ptr) {
-    mutex_lock(&bbma_mutex);
+    mutex_lock(&mutex);
     if (ptr == NULL) {
         return;
     }
@@ -337,5 +337,5 @@ void bbma_free(void* ptr) {
     char* judger = ptr + (1 << 12) - 1;;
     assert(*judger == 1);
     *judger = 0;
-    mutex_unlock(&bbma_mutex);
+    mutex_unlock(&mutex);
 }
