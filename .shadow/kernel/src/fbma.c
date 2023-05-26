e2032968a54aaf52dedaba0feacb9ed0b0d75ac5
tracer-nju <tracer@nju.edu.cn>
2023-05-26 16:07:55 +0800

    Linux appletree-virtual-machine 5.19.0-41-generic #42~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Apr 18 17:40:00 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  16:07:55 up 19 days, 14:59,  0 users,  load average: 0.22, 0.27, 0.36

--- .shadow/kernel/src/fbma.c
+++ .shadow/kernel/src/fbma.c
@@ -18,6 +18,7 @@ char origin_logg[200] = "/home/appletree/JYY-OS/kernel/test/testlog1.txt";
 BUDDY_BLOCK_STICK* buddy_blocks[BBMA_NUM];
 // spinlock_t bbma_lock[BBMA_NUM];
 spinlock_t bbma_lock = SPIN_LOCK_INIT;
+int aa = 0;
 // extern mutex_t mutex;
 
 BUDDY_BLOCK_SIZE determine_bbma_size(size_t size) {
@@ -66,6 +67,7 @@ void* get_the_free_space_by_dividing(BUDDY_BLOCK_SIZE bbma_size) {
 int ente_cnt = 0;
 void* bbma_alloc(size_t size, bool is_from_slab) {
     // mutex_lock(&mutex);
+    printf("%d", aa++);
     spin_lock(&bbma_lock);
 
     // assert(ente_cnt++==0);
