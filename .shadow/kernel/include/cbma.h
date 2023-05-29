1931d5038e41b13265ca3b2497e1567b03a37dd1
tracer-nju <tracer@nju.edu.cn>
2023-05-29 14:13:02 +0800

    Linux appletree-virtual-machine 5.19.0-42-generic #43~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Apr 21 16:51:08 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  14:13:02 up 2 days, 17:05,  1 user,  load average: 0.72, 0.66, 0.52

--- .shadow/kernel/include/cbma.h
+++ .shadow/kernel/include/cbma.h
@@ -41,16 +41,20 @@ BUDDY_BLOCK_SIZE determine_bbma_size(size_t size);
 void* bbma_alloc(size_t size);
 void* find_the_free_space_in_bbma_system(BUDDY_BLOCK_SIZE bbma_size);
 BUDDY_BLOCK_STICK* divide_larger_bbma_block_from_bbma_system(BUDDY_BLOCK_SIZE bbma_size);
-void insert_two_new_divided_child_into_bbma_system(BUDDY_BLOCK_STICK* left_divided_child, BUDDY_BLOCK_STICK* right_divided_child, BUDDY_BLOCK_SIZE bbma_size);
-void* bbma_align_to_larger_block(void* ptr, BUDDY_BLOCK_SIZE bbma_size);
 // BUDDY_BLOCK_STICK* find_the_position_where_inserting_the_free_bbma_block(BUDDY_BLOCK_STICK* inserted_bbma_block_stick, BUDDY_BLOCK_SIZE bbma_block_size);
-void insert_free_bbma_block_into_bbma_system(BUDDY_BLOCK_STICK* inserted_bbma_block_stick, BUDDY_BLOCK_SIZE bbma_block_size);
-void delete_a_free_block_in_bbma_system(BUDDY_BLOCK_STICK* block);
 void bbma_free(void* ptr);
 void bbma_init(void* start, void* end);
-void* convert_addr_to_index(void* addr);
 void* convert_index_to_addr(Tree tree, int index, BUDDY_BLOCK_SIZE cur_size);
 // void spy_insert_chain_block(BUDDY_BLOCK_STICK* position, BUDDY_BLOCK_STICK* item);
-void spy_insert_chain_block(BUDDY_BLOCK_STICK* item);
-bool judge_if_can_merge(BUDDY_BLOCK_STICK* inserted_bbma_block_stick, BUDDY_BLOCK_STICK* the_cur_bbma_expected_neighbor_block);
+Tree_Index find_available_tree(BUDDY_BLOCK_SIZE bbma_size);
+int left_child(int index);
+int right_child(int index);
+void* convert_index_to_addr(Tree tree, int index, BUDDY_BLOCK_SIZE cur_size);
+void* get_the_free_space_in_tree(Tree tree, int index, BUDDY_BLOCK_SIZE cur_size, BUDDY_BLOCK_SIZE req_size);
+void* find_the_free_space_in_bbma_system(BUDDY_BLOCK_SIZE bbma_size);
+void initialize_tree(Tree tree);
+void distribute_tree(int tree_count);
+Tree_Index determine_which_tree(void* ptr);
+void free_tree_ptr(Tree tree, int index, void* ptr, BUDDY_BLOCK_SIZE cur_size);
+
 #endif
