f6f4ebe8c62f944a49dbfeec65b9d807d489daae
tracer-nju <tracer@nju.edu.cn>
2023-05-31 19:47:32 +0800

    Linux appletree-virtual-machine 5.19.0-42-generic #43~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Apr 21 16:51:08 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  19:47:32 up 4 days, 22:40,  1 user,  load average: 0.77, 0.50, 0.99

--- .shadow/kernel/test/testcase.c
+++ .shadow/kernel/test/testcase.c
@@ -436,7 +436,7 @@ static void entry_7(int tid) {
   //     end_index --;
   //   } else {
       int size = rand() % SLAB_NUM;
-      int real_size = 1 << 6;
+      int real_size = 1 << size;
       void* ptr = pmm->alloc(real_size);
       mutex_lock(&mutex);
       remain_cap -= real_size;
