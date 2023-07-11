d3aa3becc7b35b97469569cbcaa7d024e2fc3493
tracer-nju <tracer@nju.edu.cn>
2023-07-11 20:13:00 +0800

    Linux appletree-virtual-machine 5.19.0-46-generic #47~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Jun 21 15:35:31 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  20:13:00 up  6:21,  1 user,  load average: 0.72, 0.40, 0.32

--- .shadow/sperf/sperf.c
+++ .shadow/sperf/sperf.c
@@ -85,9 +85,9 @@ char* get_prog_dir(char *command, bool need_dir) {
   if (need_dir) {
     char* program = command;
     char* path = getenv("PATH");
-    printf("path: %s\n", path);
+    // printf("path: %s\n", path);
     char* the_path = strdup(path);
-    printf("after dup: %s\n", the_path);
+    // printf("after dup: %s\n", the_path);
     // printf("%s\n", path);
     // assert(0);
     char* directory = strtok(the_path, ":");
