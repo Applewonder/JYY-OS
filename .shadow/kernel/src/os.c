376edbcba365a789ae8805f520b4c78dff7a625b
tracer-nju <tracer@nju.edu.cn>
2023-08-04 16:35:46 +0800

    Linux appletree-virtual-machine 5.19.0-46-generic #47~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Jun 21 15:35:31 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  16:35:46 up 7 days, 19:40,  1 user,  load average: 0.47, 0.52, 0.45

--- .shadow/kernel/src/os.c
+++ .shadow/kernel/src/os.c
@@ -49,7 +49,7 @@ static void os_run() {
   // for (const char *s = "Hello World from CPU #*\n"; *s; s++) {
   //   // putch(*s == '*' ? '0' + cpu_current() : *s);
   // }
-  
+  iset(true);
   while (1);
 }
 
