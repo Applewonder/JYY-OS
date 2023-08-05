4c9b55f9db1dacf1090abb9ccd694866032f8ac0
tracer-nju <tracer@nju.edu.cn>
2023-08-05 16:41:18 +0800

    Linux appletree-virtual-machine 5.19.0-46-generic #47~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Jun 21 15:35:31 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  16:41:18 up 8 days, 19:46,  1 user,  load average: 3.09, 2.28, 2.55

--- .shadow/kernel/src/os.c
+++ .shadow/kernel/src/os.c
@@ -12,8 +12,8 @@
   #define P kmt->sem_wait
   #define V kmt->sem_signal
   #define N 1
-  #define NPROD 6
-  #define NCONS 6
+  #define NPROD 1
+  #define NCONS 1
 #endif
 
 static IRQ* irq_head = NULL;
