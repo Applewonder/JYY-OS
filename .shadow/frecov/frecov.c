af84c7529bd942ebbdb3635e64b3a3f4263c7870
tracer-nju <tracer@nju.edu.cn>
2023-08-09 12:29:24 +0800

    Linux appletree-virtual-machine 5.19.0-50-generic #50-Ubuntu SMP PREEMPT_DYNAMIC Mon Jul 10 18:24:29 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  12:29:24 up 57 min,  1 user,  load average: 0.62, 0.52, 0.38

--- .shadow/frecov/frecov.c
+++ .shadow/frecov/frecov.c
@@ -290,7 +290,7 @@ bool get_pic_sha_num_and_print(u32 clu_num, Clu_Type* clu_table, char* file_name
     u32 padding_size = (4 - (line_size % 4)) % 4;
     if (padding_size == 0) {
       data = cluster;
-      printf("Hi, line 293\n");
+      // printf("Hi, line 293\n");
       break;
     }
     u32 padded_line_size = line_size + padding_size;
