70e4cc7fcf502f19d9842ae540095230f900425d
tracer-nju <tracer@nju.edu.cn>
2023-08-13 15:38:24 +0800

    Linux appletree-virtual-machine 5.19.0-50-generic #50-Ubuntu SMP PREEMPT_DYNAMIC Mon Jul 10 18:24:29 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  15:38:24 up 4 days,  2:30,  1 user,  load average: 0.51, 0.54, 0.46

--- .shadow/kernel/src/uproc.c
+++ .shadow/kernel/src/uproc.c
@@ -137,11 +137,7 @@ void* get_suitable_addr(task_t *task, void *start_addr, int length) {
             cur = cur->vm_next;
         }
     }
-    if (end_addr < task->as->area.end) {
-        return addr;
-    } else {
-        printf("start_addr: %p, length: %d, end_addr: %p\n", start_addr, length, end_addr);
-    }
+    assert(end_addr <= task->as->area.end);
     return addr;
 }
 
