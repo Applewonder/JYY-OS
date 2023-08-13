e0be92b398e669de7b0d0f9ccf877096c11fa6c2
tracer-nju <tracer@nju.edu.cn>
2023-08-13 16:56:15 +0800

    Linux appletree-virtual-machine 5.19.0-50-generic #50-Ubuntu SMP PREEMPT_DYNAMIC Mon Jul 10 18:24:29 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  16:56:15 up 4 days,  3:47,  1 user,  load average: 0.36, 0.43, 0.39

--- .shadow/kernel/src/kmt.c
+++ .shadow/kernel/src/kmt.c
@@ -249,8 +249,11 @@ Context* kmt_schedule(Event ev, Context *c) {
     // Context* ret = cpu_list[cpu_id].current_task->context[0];
     if (!fine_task) {
         ret = cpu_list[cpu_id].idle_task->context[0];
+    } else {
+        printf("task %s, nest %d\n", cpu_list[cpu_id].current_task->name, cpu_list[cpu_id].current_task->nested_interrupt);
+        panic_on(ret == NULL, "No context to schedule");
     }
-    panic_on(ret == NULL && !fine_task, "No context to schedule");
+    // panic_on(ret == NULL && !fine_task, "No context to schedule");
     return ret;
 }
 
